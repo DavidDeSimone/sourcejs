7edb211c17053172bbf4051755627943304a0cb5
David DeSimone <djd231@scarletmail.rutgers.edu>
2016-12-29 03:09:19 -0500

    Work in progress commit for branches work

--- src/tree-component.tsx
+++ src/tree-component.tsx
@@ -1,5 +1,5 @@
 declare const GitGraph;
-
+declare const Promise;
 import { Hg } from './hg.js';
 import { Source } from './repository.js';
 import _ = require('lodash');
@@ -9,7 +9,7 @@ import ReactDOM = require('react-dom');
 export module Tree {
 
     require(process.cwd() + '/gitgraph.js/build/gitgraph.min.js');
-    
+
     const myTemplateConfig = {
         colors: ["#F00", "#0F0", "#00F"], // branches colors, 1 per column
         branch: {
@@ -38,6 +38,7 @@ export module Tree {
 
     }
 
+    // @TODO update branches to be an array we iterate over
     interface State {
 
     }
@@ -67,28 +68,33 @@ export module Tree {
         tick() {
             this.repo.Branches().then(branches => {
                 _(branches).forEach(branch => {
-                    this.repo.Log(`-b ${branch}`).then(commits => {
-                        const branchState = {
-                            name: branch,
-                            parent: null, // @TODO fill this out
-                            commits: []
-                        };
-                        _(commits)
-                            .map(commit => {
-                                return {
-                                    message: commit.summary,
-                                    author: commit.user,
-                                    sha1: commit.hash
-                                }
-                            })
-                            .forEach(commit => branchState.commits.push(commit));
-                        if ((!this.state[branch]) || (this.state[branch].commits[0].sha1
-                            !== branchState.commits[0].sha1)) {
-                            const newState = {};
-                            newState[branch] = branchState;
-                            this.setState(newState);
-                        }
-                    });
+                    const getBranchCommit = this.repo.Log(`-b ${branch}`);
+                    const getParentCommit = this.repo.Log(`-r "parents(min(branch(${branch})))"`);
+                    Promise.all([getBranchCommit, getParentCommit])
+                        .then(result => {
+                            const commits = result[0];
+                            const parent = result[1];
+                            const branchState = {
+                                name: branch,
+                                parent: parent,
+                                commits: []
+                            };
+                            _(commits)
+                                .map(commit => {
+                                    return {
+                                        message: commit.summary,
+                                        author: commit.user,
+                                        sha1: commit.hash
+                                    }
+                                })
+                                .forEach(commit => branchState.commits.push(commit));
+                            if ((!this.state[branch]) || (this.state[branch].commits[0].sha1
+                                !== branchState.commits[0].sha1)) {
+                                const newState = {};
+                                newState[branch] = branchState;
+                                this.setState(newState);
+                            }
+                        });
                 });
             });
         }
@@ -99,8 +105,10 @@ export module Tree {
                     template: this.graphTemplate,
                 });
 
+                const branchMap = {};
                 _(this.state).forOwn((branch, branchName) => {
                     let graphBranch = this.graph.branch(branchName);
+                    branchMap[branchName] = graphBranch;
                     _(branch.commits).forEach(graphBranch.commit.bind(graphBranch));
                 });
             }
